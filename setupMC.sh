12009fa58277c66f29d07dad4954c7f58605d5d6
RaulRPrado <raul.prado@usp.br>
2020-04-15 09:38:35 +0200

    small edits

--- setupMC.sh
+++ setupMC.sh
@@ -20,9 +20,10 @@ export PATH=${VBF_DIR}/bin:$PATH
 
 ########
 # ROBAST
-export ROBAST_DIR="${SW_DIR}/GrOptics/v1.5.0_beta"
+# export ROBAST_DIR="${SW_DIR}/GrOptics/v1.5.0_beta"
+export ROBAST_DIR="${SW_DIR}/v1.5.0_beta"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROBAST_DIR
 
 ######
 # zstd
-export PATH=/afs/ifh.de/group/cta/VERITAS/software/bin/zstd/:${PATH}
+export PATH=/afs/ifh.de/group/cta/cta/software/zstd/:${PATH}
